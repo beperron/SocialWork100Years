c9b6b34bbbf3f1cdd260b8034aef33f1da127d95
Perron <beperron@m-c02p90v9g3qn.local>
2015-04-02 13:08:00 -0400

    Major cleaning. All functional.

---
+++ functions/journalArticleFinder.R
@@ -0,0 +1,15 @@
+journalArticleFinder.f <- function(journalTitle, keyword = FALSE, 
+                                   authorAff = FALSE, abstract = FALSE){
+  article.search <- which(full.df$record == journalTitle)
+  article.search <- full.df[article.search,]
+  article.search.ID <- select(article.search, articleID)
+  target.ID <- full.df$articleID %in% article.search.ID$articleID
+  journal.article.df <- full.df[target.ID, ]
+  journal.article.df <- filter(journal.article.df, attributes == "article")
+  journal.article.df <- arrange(journal.article.df, record)
+  if(keyword == FALSE){journal.article.df <- filter(journal.article.df, attributes != "keyWord")}
+  if(authorAff == FALSE){journal.article.df <- filter(journal.article.df, attributes != "authorAff")}
+  if(abstract == FALSE){journal.article.df <- filter(journal.article.df, attributes != "abstract")}
+  x.shortened <- gsub(" ", "", journalTitle)
+  write.csv(journal.article.df, file = paste0(x.shortened, ".csv"), row.names=F)
+}
