43380db3253887a2305d4d4daf5c0d041e05ce12
Perron <beperron@m-c02p90v9g3qn.attlocal.net>
2015-04-03 07:07:39 -0400

    Major cleanup. All functional.

--- functions/appendData.R
+++ functions/appendData.R
@@ -1,32 +1,4 @@
 appendData.f <- function(){
-    # Combines EbscoHost and ProQUest
-
-    #Creates variable with highest article ID from ebscohost database
-    ebscoIDmax<-max(ebscoBWR.df$articleID)
-
-    #Recodes article ID in ProQuest database in advance of merger with ebscohost
-    proQuestBWR.df<-mutate(proQuestBWR.df, articleID=articleID+ebscoIDmax)
-
-    full.df <- rbind(ebscoBWR.df, proQuestBWR.df)
-
-
-    year.25 <- as.character(c(1900:1988, 2014, 2015))
-
-    temp.df <- filter(full.df, attributes == "pubYear")
-
-    year.remove <- temp.df[temp.df$record %in% year.25, ]
-    year.removeID <- year.remove$articleID
-
-    full.df <- full.df[!(full.df$articleID %in% year.removeID), ]
-
-    article.count <- filter(full.df, attributes == "article") 
-    article.count.initial <<- article.count
-    journal.count <- filter(full.df, attributes == "journal")
-    
-    journal.count.initial <<- length(unique(journal.count$record))
-    
-    
-    
     
     full.df$record <- ifelse(full.df$record ==
                                  "The British Journal of Social Work",          # Spelling difference
