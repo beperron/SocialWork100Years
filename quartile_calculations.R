72019ad4bfab4353e603b6cae61256581114782b
Perron <beperron@m-c02p90v9g3qn.attlocal.net>
2015-04-03 05:51:56 -0400

    Removed files.

--- quartile_calculations.R
+++
@@ -1,9 +0,0 @@
-
-
-
-updateJournalList <- read.csv("journalList.csv")
-
-
-updateJournalList <- within(updateJournalList, quartile <- as.integer(cut(hIndex, quantile(hIndex, probs=0:4/4, na.rm=TRUE), include.lowest=TRUE)))
-
-write.csv(updateJournalList, file = "journalList.csv")
